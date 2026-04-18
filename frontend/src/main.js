dcf2495ed8f33e9fb434b4183047a7bb0e2c1616
ycb <yechengbo@miya818.com>
2026-04-18 11:54:34 +0800

    feat: add global responsive CSS for mobile adaptation

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,5 +3,6 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 import './styles/global.css'
+import './styles/responsive.css'
 
 createApp(App).use(createPinia()).use(router).mount('#app')
